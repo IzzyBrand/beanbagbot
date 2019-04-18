6dabf3f278e767b52b3728b7bd7207bcc1da344d
Izzy Brand <isaiah_brand@brown.edu>
2019-04-18 12:38:09 -0400

    added timestamp and elapsed time to command json so we can have motor timeout

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import json
 import time
-import params as p
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-cmd = {'forward': 0, 'turn': 0}
-most_recent_command_time = 0
+cmd = {'forward': 0, 'turn': 0, 'time': 0}
 
 @app.route('/')
 def main():
@@ -17,8 +15,7 @@ def main():
 @socketio.on('cmd')
 def user_input(msg):
 	cmd.update(msg)
-	most_recent_command_time = time.time()
-
+	cmd['time'] = time.time()
 
 @socketio.on('activate')
 def take_control(msg):
@@ -28,8 +25,8 @@ def take_control(msg):
 
 @app.route('/get_cmd', methods=['GET'])
 def get_cmd():
-	if time.time() - most_recent_command_time < p.server_command_timeout:
-		return json.dumps(cmd)
+	cmd['elapsed'] = time.time() - cmd['time']
+	return json.dumps(cmd)
 
 if __name__ == '__main__':
 	socketio.run(app, host='0.0.0.0', debug=True, port=5000)
